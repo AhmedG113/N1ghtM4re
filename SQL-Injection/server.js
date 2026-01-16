dc734f055b0dcf30e8faddb26b4ac23600a9d381
D3xter <154825039+AhmedG113@users.noreply.github.com>
2026-01-17 00:51:03 +0200

    Update server.js

--- SQL-Injection/server.js
+++ SQL-Injection/server.js
@@ -1,42 +1,44 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 
 const app = express();
 const db = new sqlite3.Database("./db.sqlite");
 
+// 1) middlewares لازم تيجي الأول
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// 2) static serving
 app.use(express.static("views"));
 
+// 3) routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/index.html"));
+  return res.sendFile(path.join(__dirname, "views/index.html"));
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
+  const { username, password } = req.body;
   const query = `SELECT * FROM users WHERE username='${username}' AND password='${password}'`;
-
-  console.log("[DEBUG] SQL =>", query);
 
   db.get(query, (err, row) => {
     if (row) {
+      res.cookie("auth", "1");
       return res.redirect("/home");
     }
     return res.send("ACCESS DENIED");
   });
 });
 
+// 4) protected route
 app.get("/home", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/home.html"));
+  if (req.cookies && req.cookies.auth === "1") {
+    return res.sendFile(path.join(__dirname, "views/home.html"));
+  }
+  return res.send("ACCESS DENIED");
 });
 
-app.get("/debug-db", (req, res) => {
-  db.all("SELECT * FROM users", (err, rows) => {
-    res.json(rows);
-  });
-});
-
-app.listen(1596, () => console.log("Challenge running on port 1596"));
+// 5) listening
+app.listen(1596, () => console.log("Running on 1596"));
